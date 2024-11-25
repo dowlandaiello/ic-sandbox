eeb33c83a224f408c6e153f46b4594c8ac69919b
Dowland Aiello <dowlandaiello@gmail.com>
2024-11-25 01:54:31 +0000

    Type analysis is done.

--- examples/addition.d
+++ examples/addition.d
@@ -9,20 +9,20 @@ symbol Erase: nat-, {}
 symbol Max: nat-, nat-, nat+
 symbol Aux: nat-, nat-, nat+
 
-Z >< Add(y, y)
+Z() >< Add(y, y)
 S(Add(y, t)) >< Add(y, S(t))
 
-Z >< Mult(Erase, Z)
-S(Mult(yy, Add(yyy, x))) >< Mult(Duply(yy, yyy), z)
+Z() >< Mult(Erase(), Z())
+S(Mult(yy, Add(yyy, x))) >< Mult(Dupl(yy, yyy), z)
 
-Z >< Duply(Z, Z)
-S(Duply(xx, xxx)) >< Dupl(S(xx), S(xxx))
+Z() >< Dupl(Z(), Z())
+S(Dupl(xx, xxx)) >< Dupl(S(xx), S(xxx))
 
-Z >< Erase
-S(Erase) >< Erase
+Z() >< Erase()
+S(Erase) >< Erase()
 
-Z >< Max(y, y)
+Z() >< Max(y, y)
 S(x) >< Max(Aux(x, z), z)
 
-Z >< Aux(x, S(x))
+Z() >< Aux(x, S(x))
 S(y) >< Aux(Max(y, t), S(t))
